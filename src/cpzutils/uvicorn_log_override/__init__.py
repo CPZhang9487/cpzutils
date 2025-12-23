fdda333daf7526b16c1ca089efa90bef1608c5f4
CPZhang <chenpozhang@gmail.com>
2025-12-23 16:44:52 +0800

    修改 cpzutils.uvicorn_log_override
    新增 __all__ 以移除 lint 警告

--- src/cpzutils/uvicorn_log_override/__init__.py
+++ src/cpzutils/uvicorn_log_override/__init__.py
@@ -1,1 +1,6 @@
-from .override_access_log import main as override_access_log  # type: ignore # noqa: F401
+from .override_access_log import main as override_access_log
+
+
+__all__ = [
+    "override_access_log",
+]
