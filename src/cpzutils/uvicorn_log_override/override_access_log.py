94d1d94fe2baeea2611b42107af5eae60b642906
CPZhang <chenpozhang@gmail.com>
2025-12-23 13:54:33 +0800

    修改 cpzutils.uvicorn_log_override.override_access_log
    增加 backup_count 參數
    添加是否執行過 main() 的判斷

--- src/cpzutils/uvicorn_log_override/override_access_log.py
+++ src/cpzutils/uvicorn_log_override/override_access_log.py
@@ -3,11 +3,18 @@ import logging.config
 from pathlib import Path
 
 
-def main():
+_already_overridden = False
+
+
+def main(
+    backup_count: int = 0,
+):
     """
     覆寫 uvicorn 的 access log 設定
 
-    使其加上時間標誌的同時輸出到終端和每日輪轉的日誌文件中。
+    使其加上時間標誌的同時輸出到終端和每日輪轉的日誌文件中
+
+    backup_count 參數用於設定保留的日誌文件數量，0 表示保留所有日誌文件，其餘數字表示保留的備份數量
 
     使用範例:
     ```python
@@ -35,31 +42,41 @@ def main():
 
     ```
     """
+    global _already_overridden
+    if _already_overridden:
+        return
+    _already_overridden = True
+
     Path("log").mkdir(parents=True, exist_ok=True)  # 確保 log 目錄存在
     logging.config.dictConfig(
         {
             "version": 1,
             "disable_existing_loggers": False,
             "formatters": {
-                "access": {
+                "access_plain": {
                     "()": "uvicorn.logging.AccessFormatter",
                     "use_colors": False,  # 關閉顏色以避免日誌文件中出現亂碼
+                    "fmt": '%(asctime)s - %(client_addr)s - "%(request_line)s" %(status_code)s',
+                },
+                "access_colored": {
+                    "()": "uvicorn.logging.AccessFormatter",
+                    "use_colors": True,
                     "fmt": '%(asctime)s - %(client_addr)s - "%(request_line)s" %(status_code)s',
                 },
             },
             "handlers": {
                 "access_stream": {
-                    "formatter": "access",
+                    "formatter": "access_colored",
                     "class": "logging.StreamHandler",
                     "stream": "ext://sys.stdout",
                 },
                 "access_file": {
-                    "formatter": "access",
+                    "formatter": "access_plain",
                     "class": "logging.handlers.TimedRotatingFileHandler",
                     "filename": "log/uvicorn.access.log",
                     "when": "midnight",  # 每天午夜輪轉日誌文件
                     "interval": 1,
-                    "backupCount": 0,  # 0 表示保留所有日誌文件，其餘數字表示保留的備份數量
+                    "backupCount": backup_count,
                     "encoding": "utf-8",
                 },
             },
